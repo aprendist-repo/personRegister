d3321e63cb0cd7dc47b2c25c66c2b9599b407157
admin <admin@192.168.81.128>
2015-11-13 18:34:34 -0500

    person models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_lenght=20)
+    last_name = models.CharField(max_lenght=20)
+    address = models.CharField(max_lenght=50)
+    zipcode = models.IntegerField(max_lenght=8)
+    gender = models.CharField(max_lenght=1)
+    salary = models.CharField(max_lenght=3)
